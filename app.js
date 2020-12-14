9d1108a10a7e5d34c562e6f3b9fc9630ea450de8
Geoffroy <geoffroy.periot.dev@gmail.com>
2020-12-14 12:12:03 +0100

    now end the request-response cycle prematurely

--- app.js
+++ app.js
@@ -2,24 +2,19 @@ const express = require('express');
 
 const app = express();
 
-const middleware1 = (req, res, next) => {
-  console.log("Doing stuff in middelware 1");
-  next();
+const extractUserName = (req, res, next) => {
+  req.userName = req.query.name;
+  next(); 
 }
 
-const middleware2 = (req, res, next) => {
-  console.log("Doing stuff in middleware 2");
-  next();
-}
-
-app.get('/myroute', middleware1, middleware2, (req, res) => {
+app.get('/myroute', (req, res) => {
   console.log('handling /myroute');
   res.send('content for /myroute');
 });
 
-app.get('/hello', middleware2, (req, res) => {
+app.get('/hello', extractUserName, (req, res) => {
   console.log('handling /hello');
-  res.send(`Hello ${req.query.name}`);
+  res.send(`Hello ${req.userName}`);
 });
 
 app.listen(5000, () => console.log('server listening on port 5000'));
