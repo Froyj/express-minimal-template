7ac208af6eec3d3eecb1228a383cc33a89eebcb5
Geoffroy <geoffroy.periot.dev@gmail.com>
2020-12-14 12:59:36 +0100

    now let's implement error handling

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ const express = require('express');
 const app = express();
 
 const extractUserName = (req, res, next) => {
-  req.userName = req.query.name;
-  next(); 
+  if (req.query.extractUserName) {
+    req.userName = req.query.name;
+    next();
+  } else {
+    res
+      .status(400)
+      .send('You have to specify a "name" query parameter to call this route');
+  }
 }
 
 app.get('/myroute', (req, res) => {
